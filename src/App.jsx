5b51d343e65ab0e8058813f57ea880150dd199f7
GIN Ryan-Rj (RIC-HK) <ryan-rj.gin@richemont.com>
2026-01-26 15:03:20 +0800

    Added a notes jsx file, wont have any logical use, only where Im storing my notes

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,30 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
-//import './App.css'
-//
-//function App() {
-//  const [count, setCount] = useState(0)
-//
-//  return (
-//    <>
-//      <div>
-//        <a href="https://vite.dev" target="_blank">
-//          <img src={viteLogo} className="logo" alt="Vite logo" />
-//        </a>
-//        <a href="https://react.dev" target="_blank">
-//          <img src={reactLogo} className="logo react" alt="React logo" />
-//        </a>
-//      </div>
-//      <h1>Vite + React</h1>
-//      <div className="card">
-//        <button onClick={() => setCount((count) => count + 1)}>
-//          count is {count}
-//        </button>
-//        <p>
-//          Edit <code>src/App.jsx</code> and save to test HMR
-//        </p>
-//      </div>
-//      <p className="read-the-docs">
-//        Click on the Vite and React logos to learn more
-//      </p>
-//    </>
-//  )
+import { createRoot } from 'react-dom/client'
+
+const myelement = (
+  <table>
+    <tr>
+      <th>Name</th>
+    </tr>
+    <tr>
+      <td>John</td>
+    </tr>
+    <tr>
+      <td>Elsa</td>
+    </tr>
+  </table>
+);
+
+createRoot(document.getElementById('root')).render(
+  myelement
+)
+
+//class:
+//class Car {
+//  constuctor(name) {this.brand = name;} 
+//  present(){return 'I have a ' + this.brand;}
 //}
-//
-//export default App
+//const mycar = new Car("Ford"); mycar.present();
 
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
-}
-
-export default App;
+//short form function:
+// hello = function(var) {return "Hello " + var;}
+// hello = var => "Hello " + var;
---
+++ src/notes.jsx
@@ -0,0 +1,26 @@
+//class:
+//class Car {
+//  constuctor(name) {this.brand = name;} 
+//  present(){return 'I have a ' + this.brand;}
+//}
+//const mycar = new Car("Ford"); mycar.present();
+
+//short form function:
+// hello = function(var) {return "Hello " + var;}
+// hello = var => "Hello " + var;
+// Using arrow unction makes 'this' represent the Header Object instead of the object that called the function
+
+// map() performs following action to each element:
+//const numbers = [1, 2, 3, 4];
+//const doubled == numbers.map(x => x * 2); //becomes [2, 4, 6, 8]
+//const fruitlist = ['apple', 'banana', 'cherry'];
+//
+//function MyList() {
+//  return (
+//    <ul>
+//      {fruitlist.map(fruit => 
+//        <li key={fruit}>{fruit}</li> // for every fruit, make it a bullet point, key as the fruit, dsplay fruit
+//      )}
+//    </ul>
+//  );
+//}
